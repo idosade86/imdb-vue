db5e86cb8c029e9f993182cce90111b1a04b1b8d
Ido <idokestin@gmail.com>
2019-01-17 15:03:39 +0200

    github

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="imdb-title" to="/">Enjoy IMDb</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  background: #f4f7f6;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+html{
+  font-size: 20px;
+}
+.imdb-title {
+  font-size: 30px;
+  text-decoration: none;
+  color: #33a09c;
+  margin: 25px 0px 0px 15px;
 }
 </style>
